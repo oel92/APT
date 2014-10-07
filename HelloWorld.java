939e28430c3df174ef573e9e9e665634d5c0aa55
oel92 <oliver.lo@utexas.edu>
2014-10-07 11:56:44 -0500

    testing hook

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+	public static void main( String [] args){
+		System.out.println("Hello World");
+		System.out.println("This is more than 80 char! Shouldn't work");
+	}
+
+}
