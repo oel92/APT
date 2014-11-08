c877f69cb82d75c44056aa6f106d6945d262b933
oel92 <oliver.lo@utexas.edu>
2014-11-07 19:04:18 -0600

    adding app

---
+++ connexus-os/src/com/example/connexus_os/Search.java
@@ -0,0 +1,5 @@
+package com.example.connexus_os;
+
+public class Search {
+
+}
---
+++ connexus-os/src/com/example/connexus_os/ViewStreams.java
@@ -0,0 +1,12 @@
+package com.example.connexus_os;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class ViewStreams extends ActionBarActivity {
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_streams);
+    }
+}
